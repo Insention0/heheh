efb4afc23f5453f93aa0e28971f9de76652919cf
Insention0 <mf8131692@gmail.com>
2023-04-24 09:27:21 +0200

    upload second website

--- www.optimum.net/cms-templates/plain-text/.min59c0.js
+++
@@ -1,2 +0,0 @@
-/*! optimus v0.1.0 Built at 2023-03-20 14:21 EDT */
-angular.module("plainText",["config","platformPlayer","lightBox"]).controller("PlainTextCtrl",["$scope",function(){console.log("Hello from plainText page")}]);
